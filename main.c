f7e942d2893ad0496409f7f84305bd4b4a28b6b0
peshaleesamarasinghe@gmail.com <peshaleesamarasinghe@gmail.com>
2026-09-16 16:38:29 +0530

    Add hospital header and hospital menue

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
+void hospitalHeader();
+void hospitalMenue();
+
 int main()
 {
-    printf("Smart Hospital Patient & Resource Allocation System\n");
+    hospitalHeader();
+    hospitalMenue();
+
+
     return 0;
+}
+
+void hospitalHeader()
+{
+    printf("*****************************************************************************************************************  \n");
+    printf("**    ##### ##     ##     #     ##### #######   #    #  ####  ##### ###### ####### #######     #     #         **\n");
+    printf("**    #     # #   # #    # #    #   #    #      #    # #    # #     #    #    #       #       # #    #         **\n");
+    printf("**    ##### #  # #  #   #####   #####    #      ###### #    # ##### ######    #       #      #####   #         **\n");
+    printf("**        # #   #   #  #     #  #  #     #      #    # #    #     # #         #       #     #     #  #         **\n");
+    printf("**    ##### #       # #       # #   #    #      #    #  ####  ##### #      #######    #    #       # ######    **\n");
+    printf("*****************************************************************************************************************  \n");
+    printf("                               ________________________________ \n");
+    printf("                              /               |||              \\ \n");
+    printf("                             /              |||||||             \\ \n");
+    printf("                            /         MODERN  |||  HOSPITAL      \\ \n");
+    printf("                           /______________________________________\\\n");
+    printf("                                   |                       |\n");
+    printf("                                   |                       |\n");
+    printf("                                   |        WELCOME!       |\n");
+    printf("                                   |                       |\n");
+    printf("                                   |_______________________|\n");
+    printf("  \n");
+
+
+}
+
+void hospitalMenue()
+{
+    printf("=========================================\n");
+    printf("   SMART HOSPITAL MANAGEMENT SYSTEM\n");
+    printf("=========================================\n");
+    printf("1. Register Patient\n");
+    printf("2. Search Patient\n");
+    printf("3. Exit\n");
 }
